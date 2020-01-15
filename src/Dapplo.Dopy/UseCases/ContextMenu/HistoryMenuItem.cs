19d482ac11e9e31eecc8c9c73b8ff0103ee69081
Robin Krom <robin.krom@gmail.com>
2020-01-15 14:03:13 +0100

    Fix for dotnet core 3.1 build

--- src/Dapplo.Dopy/UseCases/ContextMenu/HistoryMenuItem.cs
+++ src/Dapplo.Dopy/UseCases/ContextMenu/HistoryMenuItem.cs
@@ -1,21 +1,21 @@
 ﻿//  Dapplo - building blocks for desktop applications
 //  Copyright (C) 2016-2019 Dapplo
-// 
+//
 //  For more information see: http://dapplo.net/
 //  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
-// 
+//
 //  This file is part of Dapplo.Dopy
-// 
+//
 //  Dapplo.Dopy is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  Dapplo.Dopy is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have a copy of the GNU Lesser General Public License
 //  along with Dapplo.Dopy. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
 
@@ -64,7 +64,7 @@ namespace Dapplo.Dopy.UseCases.ContextMenu
             KeyboardHook.KeyboardEvents
                 // The hotkey to listen do
                 .Where(controlShiftPasteKey)
-#if !NETCOREAPP3_0
+#if !NETCOREAPP3_1
 // TODO: What about dotnet core 3.0?
                 // Make sure it's on the dispatcher
                 .SubscribeOnDispatcher()
